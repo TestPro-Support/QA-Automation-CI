5c6ce1f0eb39624b93c69b474bce36a0d634414f
czar-testpro <czar@testpro.io>
2023-05-14 05:19:26 +0800

    Update LoginTests

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,20 +1,20 @@
 
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import pagefactory.HomePage;
 import pagefactory.LoginPage;
 
-import java.util.logging.Logger;
-
 
 public class LoginTests extends BaseTest {
 
-
-
+    private static final Logger LOG = LoggerFactory.getLogger(LoginTests.class);
 
     @Test
     public void LoginValidEmailPasswordTest () {
+
+        LOG.debug("LoginValidEmailPasswordTest is running on Jenkins");
 
         LoginPage loginPage = new LoginPage(getDriver());
         HomePage homePage = new HomePage(getDriver());
@@ -24,12 +24,13 @@ public class LoginTests extends BaseTest {
                  .clickSubmit();
 
         Assert.assertTrue(homePage.isAvatarDisplayed());
-        System.out.println(homePage.getPlaylistName());
 
     }
 
     @Test
     public void LoginEmptyPasswordTest () {
+
+        LOG.debug("LoginEmptyPasswordTest is running on Jenkins");
 
         LoginPage loginPage = new LoginPage(getDriver());
 
